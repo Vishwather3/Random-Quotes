1c0ae0145a3a62dba21266ad3be0cf48a178e81e
Vishwather <108716221+Vishwather3@users.noreply.github.com>
2023-04-21 15:24:13 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Footer(){
+    return(
+        <div className='foot'>
+        <h1>Thank You</h1>
+        </div>
+    )
+}
---
+++ header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+export default function Header(){
+    return(
+        <div className='head'>
+        <h1>NEW QUOTES</h1>
+        <footer>BY <br></br>VISHWATHER</footer>
+        </div>
+    )
+}
